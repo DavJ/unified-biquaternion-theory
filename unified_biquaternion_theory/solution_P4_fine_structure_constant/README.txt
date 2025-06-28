b13b60b501b153f64b03b9b721c2854a906c7e73
David Jaros <jdavid.cz@gmail.com>
2025-06-28 19:18:23 +0200

    progress

--- unified_biquaternion_theory/solution_P4_fine_structure_constant/README.txt
+++ unified_biquaternion_theory/solution_P4_fine_structure_constant/README.txt
@@ -1,4 +1,1 @@
-This folder contains:
-1. A symbolic derivation sketch of the fine-structure constant based on Unified Biquaternion Theory (UBT).
-2. A Python script that calculates the known experimental value of alpha from constants.
-3. Further steps are needed to compute theoretical frequency values from the UBT Lagrangian.
+% Placeholder for README.txt
--- unified_biquaternion_theory/solution_P4_fine_structure_constant/fine_structure_numeric_estimate.py
+++ unified_biquaternion_theory/solution_P4_fine_structure_constant/fine_structure_numeric_estimate.py
@@ -1,7 +1,1 @@
-# Estimate of the fine-structure constant using physical constants
-from scipy.constants import elementary_charge as e
-from scipy.constants import hbar, c, epsilon_0
-import numpy as np
-
-alpha = e**2 / (4 * np.pi * epsilon_0 * hbar * c)
-print(f"Calculated fine-structure constant: {alpha:.12f}")
+% Placeholder for fine_structure_numeric_estimate.py
