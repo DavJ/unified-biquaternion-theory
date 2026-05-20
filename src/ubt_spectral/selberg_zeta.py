ee49af924bbc6b2f227ea45d491066c1f6c39c9c
David Jaros <jdavid.cz@gmail.com>
2026-05-21 01:45:19 +0200

    progress

---
+++ src/ubt_spectral/selberg_zeta.py
@@ -0,0 +1,21 @@
+"""
+ubt.spectral.selberg_zeta
+-------------------------
+Stub implementation for Selberg zeta function in UBT spectral module.
+"""
+
+def selberg_zeta(s, L, spectrum):
+    """
+    Compute the Selberg zeta function Z(s) for a given length spectrum.
+    This is a stub; replace with full implementation as needed.
+
+    Parameters:
+        s (complex): Argument of the zeta function.
+        L (list of float): List of primitive geodesic lengths.
+        spectrum (list of float): Eigenvalues or related spectral data.
+
+    Returns:
+        float: Value of the Selberg zeta function (stub: returns 1.0).
+    """
+    # TODO: Implement full Selberg zeta function
+    return 1.0
